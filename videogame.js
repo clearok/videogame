6f6a546e7055c1805690a472d13dcd8f4ca6c1ac
Cesario <clrwxb@gmail.com>
2020-12-06 20:40:25 -0800

    test

---
+++ videogame.js
@@ -0,0 +1,34 @@
+class Player {
+    // class properties (attributes)
+    constructor(name, health, energy, armor) {
+      this.name = name;
+      this.health = health;
+      this.energy = energy;
+      this.armor = armor;
+    }
+    
+    // getter
+    get hp () {
+
+    }
+
+    // setter
+    set hp () {
+        
+    }
+  }
+  
+  
+//   A private health variable hp. Make sure it's private!
+//   A private maxHealth variable maxHp. Again, make sure it's private.
+//   A private energy variable en. Again, make sure it's private.
+//   A private maxEnergy variable maxEn. You know the drill by now.
+  
+  
+//   Your Player instances will be constructed as new Player(name,health,energy,armor)
+  
+  
+//   A private health variable hp. Make sure it's private!
+//   A private maxHealth variable maxHp. Again, make sure it's private.
+//   A private energy variable en. Again, make sure it's private.
+//   A private maxEnergy variable maxEn. You know the drill by now.
